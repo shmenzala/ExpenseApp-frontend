6b8cfbad95f54c1fdb84a0e51e153c5a6ec440a4
Samuel <shmenzala@gmail.com>
2024-07-03 19:37:37 -0500

    [ADD] Añadir configuración inicial de la aplicación (Module)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { GastosService } from './gastos.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GastosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
